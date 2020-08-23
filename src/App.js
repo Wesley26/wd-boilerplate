38d3101a4925f11ddeacf194fec218f3a7abcf49
Wesley <Gamer@DESKTOP-PQIIC82>
2020-08-23 00:46:04 -0400

    08/23/2020 Commit
    08/23/2020 Commit -
    Added to App.css, App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ function App() {
   return (
     <div className="App">
       <div className="Main">
-        <p>Hello World! This is demo text.</p>
+        <h1>Hello World! This is demo text title.</h1>
+        <div className ="ContentWrapper">
+          <div className="NavWrapper">
+            <h3>
+              This is Nav bar.
+            </h3>
+          </div>
+          <div className="ContentA">
+              <h3>
+                This is demo text paragraph. Here, this paragraph of information describes your subject
+                in greater detail.
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                mollit anim id est laborum.
+              </p>
+          </div>
+        </div>
       </div>
     </div>
   );
